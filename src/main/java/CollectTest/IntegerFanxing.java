92d5c5a1b14a5a03dac79c3b08eb3e3813cda141
张国杨Zero <101501786+ZhangGuoYangZero@users.noreply.github.com>
2022-05-08 22:15:40 +0800

    Update IntegerFanxing.java

--- src/main/java/CollectTest/IntegerFanxing.java
+++ src/main/java/CollectTest/IntegerFanxing.java
@@ -9,7 +9,9 @@ public class IntegerFanxing implements Comparable<IntegerFanxing>{
     public  IntegerFanxing(Integer m ){
         n = m;
     }
-    //只要继承了 compareable接口，并且实现了compareTo方法，在进入有HASH系列的集合中会自动调用对应的compareTo方法
+    //只要继承了 compareable接口，并且实现了compareTo方法，
+    // 在进入有HASH系列的集合中会自动调用对应的compareTo方法
+    //默认自然排序就是从小到大 ，如果返回1就交换
     @Override
     public int compareTo(IntegerFanxing o) {
         int a  = this.n.compareTo(o.n);
